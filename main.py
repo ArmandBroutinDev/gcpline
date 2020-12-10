9ae35ea053570e172c6f18d40c56f75ff6c0f11f
Armand B <71330618+ArmandBroutinDev@users.noreply.github.com>
2020-12-10 11:14:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -36,22 +36,40 @@ class DataHandler:
 class FeatureRecipe:
 
     def __init__(self,df:pd.DataFrame):
+        print("FeatureRecipe intialisation")
         self.df = df
-        self.cate = None
-        self.floa = None
-        self.intt = None
-        pass
+        self.cate = []
+        self.floa = []
+        self.intt = []
+        print("end of intialisation")
 
     def separate_variable_types(self) -> None:
-        #return cate,floa,intt
-        pass
-    def drop_na_prct(self,threshold: float):
+        for col in self.df.columns:
+            if self.df[col].dtypes == int:
+                self.intt.append(self.df[col])
+            elif self.df[col].dtypes == float:
+                self.floa.append(self.df[col])
+            else:
+                self.cate.append(self.df[col])
+        print ("dataset column size : {} \nnumber of discreet values : {} \nnumber of continuous values : {} \nnumber of others : {} \ntaille total : {}".format(len(self.df.columns),len(self.intt),len(self.floa),len(self.cate),len(self.intt)+len(self.floa)+len(self.cate) ))
+
+    def drop_na_prct(self,threshold : float):
+        # par rapport a la colonne
         pass
     def drop_useless_features(self):
-        pass
+        # col vides et doublons de l'index et les colonnes qu'on va considerer inutile
+        for col in self.df.columns:
+            if self.df[col] == self.df.index:
+                print()
+
     def drop_duplicate(self):
+        # comparer les colonnes et voir si elles sont dupliquées
         pass
 #   def deal_date_time(self):
 #        pass
-    def get_process_data(self,threshold:float):
-        pass
+    def get_process_data(self,threshold : float):
+        self.separate_variable_types()
+        self.drop_na_prct(threshold)
+        self.drop_useless_features()
+        self.drop_duplicate()
+        print("end of FeatureRecipe processing")
